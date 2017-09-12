856ec850c4448ade3f7e125febafa374059e4474
Thien Nguyen <tatthien.contact@gmail.com>
2017-09-12 22:44:26 +0700

    Add default admin account

--- install.go
+++ install.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/joho/godotenv"
 	"github.com/tatthien/go-cms-api/database"
+	"github.com/tatthien/go-cms-api/model"
 )
 
 func main() {
@@ -32,4 +40,28 @@ func main() {
 		"FOREIGN KEY (`author_id`) REFERENCES users(`id`)" +
 		") ENGINE=InnoDB DEFAULT CHARSET=utf8"
 	db.CreateTable("posts", query)
+
+	// Insert admin account
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var user model.User
+	user.Username = os.Getenv("ADMIN_USERNAME")
+	user.Email = os.Getenv("ADMIN_EMAIL")
+	password := os.Getenv("ADMIN_PASSWORD")
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	checkError(err)
+
+	user.Password = string(hash)
+
+	admin, err := db.InsertUser(user)
+	checkError(err)
+	fmt.Println(admin)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
